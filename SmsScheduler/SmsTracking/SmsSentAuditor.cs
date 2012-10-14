ceaa390c7893a45738a362c6e7ac17668e5bd243
Sam Jeffress <samjeffress@gmail.com>
2012-10-14 15:48:10 +1100

    Started adding SmsTracking

---
+++ SmsScheduler/SmsTracking/SmsSentAuditor.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+using Raven.Client;
+using SmsMessages.Events;
+
+namespace SmsTracking
+{
+    public class SmsSentAuditor : IHandleMessages<MessageSent>
+    {
+        public IDocumentStore DocumentStore { get; set; }
+
+        public void Handle(MessageSent message)
+        {
+            using (var session = DocumentStore.OpenSession())
+            {
+                var messageSent = session.Load<MessageSent>(message.ConfirmationData.Receipt);
+                if (messageSent != null) return;
+                session.Store(message, message.ConfirmationData.Receipt);
+                session.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SmsScheduler/SmsTrackingTests/SmsSentAuditorTestFixture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Embedded;
+using SmsMessages.CommonData;
+using SmsMessages.Events;
+using SmsTracking;
+
+namespace SmsTrackingTests
+{
+    [TestFixture]
+    public class SmsSentAuditorTestFixture
+    {
+        private IDocumentStore _documentStore;
+
+        [SetUp]
+        public void Setup()
+        {
+            _documentStore = new EmbeddableDocumentStore {DefaultDatabase = "SmsTracking"}.Initialize();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _documentStore.Dispose();
+        }
+
+        [Test]
+        public void HandleMessageSent()
+        {
+            var messageSent = new MessageSent { ConfirmationData = new SmsConfirmationData("receipt", DateTime.Now.AddMinutes(-10), 0.33m) };
+            var smsSentAuditor = new SmsSentAuditor { DocumentStore = _documentStore };
+            smsSentAuditor.Handle(messageSent);
+
+            using (var session = _documentStore.OpenSession())
+            {
+                var savedMessage = session.Load<MessageSent>("receipt");
+                Assert.That(savedMessage, Is.Not.Null);
+            }
+        }
+    }
+}
