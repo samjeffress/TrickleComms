a307e69546e3a0df8d01f104b5ba4178569d7afc
Sam Jeffress <samjeffress@gmail.com>
2012-10-16 16:23:53 +1100

    started adding CoordinatorMessageTracking

--- SmsScheduler/SmsTracking/CoordinatorTracker.cs
+++ SmsScheduler/SmsTracking/CoordinatorTracker.cs
@@ -1,20 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using NServiceBus;
 using Raven.Client;
 
 namespace SmsTracking
 {
     public class CoordinatorTracker : 
-        IHandleMessages<CoordinatorOverTimeCreated>
+        IHandleMessages<CoordinatorCreated>
     {
         public IDocumentStore DocumentStore { get; set; }
 
-        public void Handle(CoordinatorOverTimeCreated message)
+        public void Handle(CoordinatorCreated message)
         {
-            throw new System.NotImplementedException();
+            using(var session = DocumentStore.OpenSession())
+            {
+                var coordinatorTrackingData = new CoordinatorTrackingData();
+                coordinatorTrackingData.CoordinatorId = message.CoordinatorId;
+                coordinatorTrackingData.MessageStatuses = message
+                    .ScheduledMessages
+                    .Select(s => new MessageSendingStatus { Number = s.Number, ScheduledSendingTime = s.ScheduledTime })
+                    .ToList();
+                session.Store(coordinatorTrackingData, message.CoordinatorId.ToString());
+                session.SaveChanges();
+            }
         }
     }
 
-    public class CoordinatorOverTimeCreated
+    public class CoordinatorCreated
     {
+        public Guid CoordinatorId { get; set; }
+
+        public List<MessageSchedule> ScheduledMessages { get; set; }
+    }
+
+    public class MessageSchedule
+    {
+        public string Number { get; set; }
+
+        public DateTime ScheduledTime { get; set; }
+    }
+
+    public class CoordinatorTrackingData
+    {
+        public Guid CoordinatorId { get; set; }
+
+        public CoordinatorStatusTracking CurrentStatus { get; set; }
+
+        public List<MessageSendingStatus> MessageStatuses { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+    }
+
+    public class MessageSendingStatus
+    {
+        public string Number { get; set; }
+
+        public DateTime ScheduledSendingTime { get; set; }
+
+        public MessageStatusTracking Status { get; set; }
+
+        public Decimal? Cost { get; set; }
+
+        public DateTime? ActualSentTime { get; set; }
+    }
+
+    public enum MessageStatusTracking
+    {
+        Scheduled,
+        Paused,
+        Completed
+    }
+
+    public enum CoordinatorStatusTracking
+    {
+        Started,
+        Paused,
+        Completed
     }
 }
--- SmsScheduler/SmsTrackingTests/SmsCoordinatorTestFixture.cs
+++ SmsScheduler/SmsTrackingTests/SmsCoordinatorTestFixture.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using SmsTracking;
 
 namespace SmsTrackingTests
 {
@@ -6,9 +9,29 @@ namespace SmsTrackingTests
     public class SmsCoordinatorTestFixture : RavenTestBase
     {
         [Test]
-        public void TrickleOverTimeSetup()
+        public void CoordinateMessagesCreated()
         {
-            
+            var coordinatorCreated = new CoordinatorCreated { CoordinatorId = Guid.NewGuid(), ScheduledMessages = new List<MessageSchedule> { 
+                new MessageSchedule { Number = "04040044", ScheduledTime = DateTime.Now.AddMinutes(5)},
+                new MessageSchedule { Number = "07777777", ScheduledTime = DateTime.Now.AddMinutes(10)} 
+            }};
+
+            var coordinatorTracker = new CoordinatorTracker { DocumentStore = DocumentStore };
+            coordinatorTracker.Handle(coordinatorCreated);
+
+            using (var session = DocumentStore.OpenSession())
+            {
+                var coordinatorTrackingData = session.Load<CoordinatorTrackingData>(coordinatorCreated.CoordinatorId.ToString());
+                Assert.That(coordinatorTrackingData.CurrentStatus, Is.EqualTo(CoordinatorStatusTracking.Started));
+                Assert.That(coordinatorTrackingData.CoordinatorId, Is.EqualTo(coordinatorCreated.CoordinatorId));
+                Assert.That(coordinatorTrackingData.MessageStatuses.Count, Is.EqualTo(2));
+                Assert.That(coordinatorTrackingData.MessageStatuses[0].Number, Is.EqualTo(coordinatorCreated.ScheduledMessages[0].Number));
+                Assert.That(coordinatorTrackingData.MessageStatuses[0].ScheduledSendingTime, Is.EqualTo(coordinatorCreated.ScheduledMessages[0].ScheduledTime));
+                Assert.That(coordinatorTrackingData.MessageStatuses[0].Status, Is.EqualTo(MessageStatusTracking.Scheduled));
+                Assert.That(coordinatorTrackingData.MessageStatuses[1].Number, Is.EqualTo(coordinatorCreated.ScheduledMessages[1].Number));
+                Assert.That(coordinatorTrackingData.MessageStatuses[1].ScheduledSendingTime, Is.EqualTo(coordinatorCreated.ScheduledMessages[1].ScheduledTime));
+                Assert.That(coordinatorTrackingData.MessageStatuses[1].Status, Is.EqualTo(MessageStatusTracking.Scheduled));
+            }
         }
     }
 }
