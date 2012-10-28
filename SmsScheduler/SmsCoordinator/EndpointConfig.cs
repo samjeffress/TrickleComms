ba9d88956f6e1036e2e406efb59847b746e7e5ef
Sam Jeffress <samjeffress@gmail.com>
2012-10-28 20:33:46 +1100

    coordinator endpoint config

--- SmsScheduler/SmsCoordinator/EndpointConfig.cs
+++ SmsScheduler/SmsCoordinator/EndpointConfig.cs
@@ -2,7 +2,26 @@
 
 namespace SmsCoordinator
 {
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher
+    public class EndpointConfig : IConfigureThisEndpoint, IWantCustomInitialization
     {
+        public void Init()
+        {
+            var configure = Configure.With()
+            .DefaultBuilder()
+                .Log4Net()
+            .XmlSerializer()
+            .MsmqTransport()
+                .IsTransactional(true)
+                .PurgeOnStartup(false)
+            .UnicastBus()
+                .LoadMessageHandlers();
+
+            Configure.Instance.Configurer.ConfigureComponent<SmsService>(DependencyLifecycle.InstancePerUnitOfWork);
+            Configure.Instance.Configurer.ConfigureComponent<TwilioWrapper>(DependencyLifecycle.InstancePerUnitOfWork);
+            Configure.Instance.Configurer.ConfigureComponent<CalculateSmsTiming>(DependencyLifecycle.InstancePerUnitOfWork);
+
+            configure.CreateBus()
+            .Start(() => Configure.Instance.ForInstallationOn<NServiceBus.Installation.Environments.Windows>().Install());
+        }
     }
 }
