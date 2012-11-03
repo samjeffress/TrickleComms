eab7a204bbea5585e09f768839845a4d466a7ce5
Sam Jeffress <samjeffress@gmail.com>
2012-11-03 21:00:22 +1100

    added ScheduleController - not implementated

---
+++ SmsScheduler/SmsWeb/Controllers/ScheduleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using NServiceBus;
+using SmsWeb.Models;
+
+namespace SmsWeb.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public IBus Bus { get; set; }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(ScheduleModel schedule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmsScheduler/SmsWeb/Models/ScheduleModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmsWeb.Models
+{
+    public class ScheduleModel
+    {
+        [Required]
+        public string Number { get; set; }
+
+        [Required]
+        public string MessageBody { get; set; }
+
+        public string ConfirmationEmail { get; set; }
+
+        [Required]
+        public DateTime ScheduledTime { get; set; }
+
+        public Guid ScheduleMessageId { get; set; }
+    }
+}
