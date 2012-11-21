07fd60f9723dc2beaad5cb54f93785e06b47043a
Sam Jeffress <samjeffress@gmail.com>
2012-11-21 19:38:16 +1100

    added servicestack to SmsWeb, setup NServiceBus through its IOC, got simple api working

---
+++ SmsScheduler/SmsWeb/API/Sms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NServiceBus;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.ServiceModel;
+using SmsMessages.MessageSending;
+
+namespace SmsWeb.API
+{
+    public class Sms
+    {
+        public Guid RequestId { get; set; }
+
+        public string Number { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class SmsResponse : IHasResponseStatus
+    {
+        public Guid RequestId { get; set; }
+
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+
+    public class SmsService : RestServiceBase<Sms>
+    {
+        public IBus Bus { get; set; }
+
+        public override object OnGet(Sms request)
+        {
+            var requestId = Guid.NewGuid();
+            Bus.Send(new SendOneMessageNow {CorrelationId = requestId});
+            return new SmsResponse {RequestId = requestId};
+        }
+    }
+}
