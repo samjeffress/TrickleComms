18e0bca32e6e62d0b76b9f5282806f1dd4840927
Sam Jeffress <samjeffress@gmail.com>
2012-12-01 14:33:18 +1100

    set default route, deleted unused default.htm and set views to compile

--- SmsScheduler/SmsWeb/App_Start/RouteConfig.cs
+++ SmsScheduler/SmsWeb/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace SmsWeb.App_Start
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Coordinator", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
