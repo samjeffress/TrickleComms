13b35c1649e11f8a6b36164ae133a90a73ac0f10
Sam Jeffress <samjeffress@gmail.com>
2012-12-25 18:12:06 +1100

    added RavenDocStore to EmailSender, setting up EndpointConfig

---
+++ SmsScheduler/EmailSender/RavenDocStore.cs
@@ -0,0 +1,27 @@
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Extensions;
+
+namespace EmailSender
+{
+    public interface IRavenDocStore
+    {
+        IDocumentStore GetStore();
+    }
+
+    public class RavenDocStore : IRavenDocStore
+    {
+        private readonly IDocumentStore _documentStore;
+        public RavenDocStore()
+        {
+            _documentStore = new DocumentStore { Url = "http://localhost:8080", DefaultDatabase = "SmsTracking" };
+            _documentStore.Initialize();
+            _documentStore.DatabaseCommands.EnsureDatabaseExists("MailGunConfiguration");
+        }
+
+        public IDocumentStore GetStore()
+        {
+            return _documentStore;
+        }
+    }
+}
