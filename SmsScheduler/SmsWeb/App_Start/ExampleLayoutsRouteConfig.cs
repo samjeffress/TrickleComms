b86f21807beac230b33fd2abad74485b2e2a8dc2
Sam Jeffress <samjeffress@gmail.com>
2013-01-12 13:46:34 +1100

    mostly moved to twitter bootstrap, created navigation headers and moved layouts

--- SmsScheduler/SmsWeb/App_Start/ExampleLayoutsRouteConfig.cs
+++ SmsScheduler/SmsWeb/App_Start/ExampleLayoutsRouteConfig.cs
@@ -9,18 +9,19 @@ namespace BootstrapMvcSample
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapNavigationRoute<HomeController>("Home", c => c.Index());
             routes.MapNavigationRoute<SendNowController>("Send Now", c => c.Create());
             routes.MapNavigationRoute<ScheduleController>("Schedule", c => c.Create());
-            routes.MapNavigationRoute<CoordinatorController>("Coordinate", c => c.Create());
-            routes.MapNavigationRoute<CoordinatorController>("Coordination History", c => c.History(0, 20));
-            routes.MapNavigationRoute<HomeController>("Configuration", c => c.Configuration());
+            routes.MapNavigationRoute<CoordinatorController>("Coordinate", c => c.Index())
+                .AddChildRoute<CoordinatorController>("Create", c => c.Create())
+                .AddChildRoute<CoordinatorController>("History", c => c.History(0, 20));
+            routes.MapNavigationRoute<HomeController>("Configuration", c => c.Configuration())
+                .AddChildRoute<TwilioConfigController>("Twilio", t => t.Details())
+                .AddChildRoute<MailgunConfigController>("Mailgun", m => m.Details());
 
             routes.MapNavigationRoute<ExampleLayoutsController>("Example Layouts", c => c.Starter())
                   .AddChildRoute<ExampleLayoutsController>("Marketing", c => c.Marketing())
                   .AddChildRoute<ExampleLayoutsController>("Fluid", c => c.Fluid())
-                  .AddChildRoute<ExampleLayoutsController>("Sign In", c => c.SignIn())
-                ;
+                  .AddChildRoute<ExampleLayoutsController>("Sign In", c => c.SignIn());
         }
     }
 }
