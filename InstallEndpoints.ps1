80a75d55f6b45635b0e9855f2da34fddf384fb55
Sam Jeffress <samjeffress@gmail.com>
2013-02-21 19:00:03 +1100

    added unit tests to Setup, added config for output in Release for test projects, added NUnit.Runner to project

--- InstallEndpoints.ps1
+++ InstallEndpoints.ps1
@@ -12,14 +12,14 @@ if ([System.IO.Directory]::Exists($installFolder))
 [System.IO.Directory]::Move('.\build_output\SmsTracking', $installFolder + '\SmsTracking')
 
 $nsbHost = $installFolder + '\SmsCoordinator\NServiceBus.Host.exe'
-& $nsbHost ("/install", "/serviceName:SmsEmailSender", "/displayName:Sms Email Sender", "/description:Service for sending emails from Sms Coordinator")
+& $nsbHost ("/install", "/serviceName:SmsEmailSender", "/displayName:Sms Email Sender", "/description:Service for sending emails from Sms Coordinator", "NServiceBus.Production")
 Start-Service SmsEmailSender
 
 $nsbHost = $installFolder + '\SmsCoordinator\NServiceBus.Host.exe'
-& $nsbHost ("/install", "/serviceName:SmsCoordinator", "/displayName:Sms Coordinator", "/description:Service for coordinating and sending Sms")
+& $nsbHost ("/install", "/serviceName:SmsCoordinator", "/displayName:Sms Coordinator", "/description:Service for coordinating and sending Sms", "NServiceBus.Production")
 Start-Service SmsCoordinator
 
 #.\build_output\SmsTracking\NServiceBus.Host.exe /install /serviceName:"SmsTracking" /displayName:"Sms Tracking" /description:"Service for tracking status of coordinated and Sms"
 $nsbHost = $installFolder + '\SmsTracking\NServiceBus.Host.exe'
-& $nsbHost ("/install", "/serviceName:SmsTracking", "/displayName:Sms Tracking",  "/description:Service for tracking status of coordinated and Sms")
+& $nsbHost ("/install", "/serviceName:SmsTracking", "/displayName:Sms Tracking",  "/description:Service for tracking status of coordinated and Sms", "NServiceBus.Production")
 Start-Service SmsTracking
--- Setup.bat
+++ Setup.bat
@@ -1,9 +1,11 @@
 powershell.exe Set-ExecutionPolicy remotesigned
-powershell.exe .\UninstallEndpoints.ps1
-powershell.exe Set-ExecutionPolicy remotesigned
 powershell.exe .\Build.ps1
 powershell.exe Set-ExecutionPolicy remotesigned
+powershell.exe .\RunTests.ps1
+powershell.exe Set-ExecutionPolicy remotesigned
 powershell.exe .\SetupInfrastructure.ps1
+powershell.exe Set-ExecutionPolicy remotesigned
+powershell.exe .\UninstallEndpoints.ps1
 powershell.exe Set-ExecutionPolicy remotesigned
 powershell.exe .\InstallEndpoints.ps1
 powershell.exe Set-ExecutionPolicy remotesigned
