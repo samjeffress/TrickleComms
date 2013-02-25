59739ae3824014886fbb6971fa486e4453c8332d
Sam Jeffress <samjeffress@gmail.com>
2013-02-25 15:01:14 +1100

    RunTests now using paths relative to script directory

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,2 +1,16 @@
-$nunit = ".\SmsScheduler\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe"
-& $nunit .\build_output\tests\EmailSenderTests.dll .\build_output\tests\SmsCoordinatorTests.dll .\build_output\tests\SmsTrackingTests.dll .\build_output\tests\SmsWebTests.dll
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$path = Get-ScriptDirectory RunTests.ps1
+
+$nunit = $path + "\SmsScheduler\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe"
+
+$EmailSender = Join-Path $path -childpath "\build_output\tests\EmailSenderTests.dll"
+$SmsCoordinatorTests = Join-Path $path -childpath "\build_output\tests\SmsCoordinatorTests.dll"
+$SmsTrackingTests = Join-Path $path -childpath "\build_output\tests\SmsTrackingTests.dll"
+$SmsWebTests = Join-Path $path -childpath "\build_output\tests\SmsWebTests.dll"
+
+& $nunit $EmailSender $SmsCoordinatorTests $SmsTrackingTests $SmsWebTests 
