d7d564b13f878b59c45c86994e9d1396a595d88b
Sam Jeffress <samjeffress@gmail.com>
2013-03-04 14:28:25 +1100

    setting path to SetParameters.xml file properly

--- SmsScheduler/Installer.ps1
+++ SmsScheduler/Installer.ps1
@@ -66,6 +66,7 @@ function InstallWeb
     $msDeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V2\msdeploy.exe"
     $webDeployPackage = Join-Path $build_output -childpath '\SmsWeb.zip'
     
+    $environmentParametersFile
     if ($go_environment -ne $null)
     {
         Write-Host "Go environment set to $go_environment, copying appropriate web.config"
@@ -79,7 +80,7 @@ function InstallWeb
     }
     
     echo $webDeployPackage
-    $arg = " -verb:sync -source:package='$webDeployPackage' -dest:auto -verbose -setParamFile=SmsWeb.SetParameters.xml"
+    $arg = " -verb:sync -source:package='$webDeployPackage' -dest:auto -verbose -setParamFile='$environmentParametersFile'"
     
     echo $arg
     #$arg = ""
