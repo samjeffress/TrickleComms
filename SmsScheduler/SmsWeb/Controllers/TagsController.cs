33df9344df59ba4f9b570977fee83ebd66441531
Sam Jeffress <samjeffress@gmail.com>
2013-03-26 16:47:17 +1100

    initial commit to have a more tag-like experience, rather than csv - just in coordinator ui at the moment

---
+++ SmsScheduler/SmsWeb/Controllers/TagsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SmsWeb.Controllers
+{
+    public class TagsController : Controller
+    {
+        public JsonResult TagList()
+        {
+            var keyValuePairs = new List<TagInfo>
+            {
+                new TagInfo { Id = "one", Name = "one" }, 
+                new TagInfo { Id = "two", Name = "two" }
+            };
+            return Json(keyValuePairs, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class TagInfo
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
