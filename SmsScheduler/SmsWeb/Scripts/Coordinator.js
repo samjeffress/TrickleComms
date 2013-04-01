9b559eca47a1f3f2cfd6b0e6ecc8b4918a50d2fc
Sam Jeffress <samjeffress@gmail.com>
2013-04-01 19:41:45 +1100

    added AllAtOnce button, styling and logic

--- SmsScheduler/SmsWeb/Scripts/Coordinator.js
+++ SmsScheduler/SmsWeb/Scripts/Coordinator.js
@@ -4,6 +4,7 @@ function ShowSendAll() {
     $('#SendAllByDiv').show(200);
     $('#TimeSeparatorSeconds').val('');
     $('#SendAllAtOnce').val(false);
+    SetHighlightedButton("#SendAllByButton");
 }
 
 function ShowTimeSeparated() {
@@ -11,6 +12,7 @@ function ShowTimeSeparated() {
     $('#TimeSeparatedDiv').show(200);
     $('#SendAllBy').val('');
     $('#SendAllAtOnce').val(false);
+    SetHighlightedButton("#TimeSeparatedButton");
 }
 
 function HideTimingOptions() {
@@ -18,16 +20,35 @@ function HideTimingOptions() {
     $('#TimeSeparatedDiv').hide(200);
     $('#SendAllBy').val('');
     $('#SendAllAtOnce').val(true);
+    SetHighlightedButton("#AllAtOnceButton");
 }
 
 function CheckSendAll() {
     if ($('#SendAllBy').val().length == 0) {
         $('#SendAllByDiv').hide();
+    } else {
+        ShowSendAll();
     }
 }
 
 function CheckTimeSeparated() {
     if ($('#TimeSeparatorSeconds').val().length == 0) {
         $('#TimeSeparatedDiv').hide();
+    } else {
+        ShowTimeSeparated();
     }
+}
+
+function SetHighlightedButton(idToHighlight) {
+
+    $('#SendAllByButton').removeClass("SelectedButton");
+    $('#SendAllByButton').addClass("UnSelectedButton");
+    $('#TimeSeparatedButton').removeClass("SelectedButton");
+    $('#TimeSeparatedButton').addClass("UnSelectedButton");
+    $('#AllAtOnceButton').removeClass("SelectedButton");
+    $('#AllAtOnceButton').addClass("UnSelectedButton");
+    
+
+    $(idToHighlight).removeClass("UnSelectedButton");
+    $(idToHighlight).addClass("SelectedButton");
 }
