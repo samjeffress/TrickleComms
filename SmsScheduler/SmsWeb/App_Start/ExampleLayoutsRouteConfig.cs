668a01f1be0888e20361e4c6929d7d04157e97b9
Sam Jeffress <samjeffress@gmail.com>
2013-04-10 21:27:42 +1000

    removing sendnow and schedule from web - all messages go through coordinator

--- SmsScheduler/SmsWeb/App_Start/ExampleLayoutsRouteConfig.cs
+++ SmsScheduler/SmsWeb/App_Start/ExampleLayoutsRouteConfig.cs
@@ -8,12 +8,9 @@ namespace BootstrapMvcSample
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapNavigationRoute<SendNowController>("Send Now", c => c.Create());
-            routes.MapNavigationRoute<ScheduleController>("Schedule", c => c.Create());
-            routes.MapNavigationRoute<CoordinatorController> ("Coordinate", c  => c.Index())
-                .AddChildRoute<CoordinatorController>("Create", c => c.Create())
-                .AddChildRoute<CoordinatorController>("History", c => c.History(0, 20));
-            //routes.MapNavigationRoute("tests", "api", "api");
+            routes.MapNavigationRoute<CoordinatorController>("Create", c => c.Create());
+            routes.MapNavigationRoute<CoordinatorController>("History", c => c.History(0, 20));
+            routes.MapNavigationRoute("API", "api", "api");
             routes.MapNavigationRoute<HomeController>("Configuration", c => c.Configuration())
                 .AddChildRoute<TwilioConfigController>("Twilio", t => t.Details())
                 .AddChildRoute<MailgunConfigController>("Mailgun", m => m.Details())
