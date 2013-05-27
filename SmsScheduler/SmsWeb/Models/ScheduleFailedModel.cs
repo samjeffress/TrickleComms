2de37fd62a38014b5417e37442fcd5ac9898f953
Sam Jeffress <samjeffress@gmail.com>
2013-05-27 16:12:21 +1000

    added coordinator failed messages details with paging to coordinator details

---
+++ SmsScheduler/SmsWeb/Models/ScheduleFailedModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SmsWeb.Models
+{
+    public class ScheduleFailedModel
+    {
+        public Guid ScheduleId { get; set; }
+
+        public string Number { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string ErrorMoreInfo { get; set; }
+
+        public string ErrorCode { get; set; }
+    }
+}
