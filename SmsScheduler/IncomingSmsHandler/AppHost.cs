db7d67b07569d174b7e21245f5b8ed7545384469
sam jeffress <samjeffress@gmail.com>
2013-11-30 21:31:38 +1100

    added api for incoming messages

---
+++ SmsScheduler/IncomingSmsHandler/AppHost.cs
@@ -0,0 +1,16 @@
+﻿using Funq;
+using ServiceStack.WebHost.Endpoints;
+using SmsMessages.MessageSending.Events;
+
+namespace IncomingSmsHandler
+{
+    public class AppHost : AppHostHttpListenerBase
+    {
+        public override void Configure(Container container)
+        {
+            // TODO: wire up bus for IOC
+            Routes
+                .Add<MessageReceived>("/SmsIncoming");
+        }
+    }
+}
---
+++ SmsScheduler/IncomingSmsHandler/ReceivedMessageService.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+using ServiceStack.ServiceHost;
+using SmsMessages.MessageSending.Events;
+
+namespace IncomingSmsHandler
+{
+    public class ReceivedMessageService : IService
+    {
+        public IBus Bus { get; set; }
+
+        public void Post(MessageReceived request)
+        {
+            Bus.Send(request);
+        }
+    }
+}
