e5d24ada7d02ecd09d74990bcece953321f90ed8
Sam Jeffress <samjeffress@gmail.com>
2013-12-03 10:52:13 +1100

    removed auth from smsactioner http api

--- SmsScheduler/SmsActioner/SmsReceivedHandler.cs
+++ SmsScheduler/SmsActioner/SmsReceivedHandler.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using NServiceBus;
-using ServiceStack.CacheAccess;
-using ServiceStack.CacheAccess.Providers;
+﻿using NServiceBus;
 using ServiceStack.ServiceInterface;
-using ServiceStack.ServiceInterface.Auth;
 using ServiceStack.WebHost.Endpoints;
 using SmsMessages.MessageSending.Commands;
 
@@ -26,86 +21,8 @@ namespace SmsActioner
 
         public override void Configure(Funq.Container container)
         {
-            Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] { new BasicAuthProvider() }));
-            Plugins.Add(new RegistrationFeature());
-            container.Register<ICacheClient>(new MemoryCacheClient());
-            var userRep = new BasicAuthImpl();
-            container.Register<IUserAuthRepository>(userRep);
             Routes
                 .Add<MessageReceived>("/MessageReceived/");
-        }
-    }
-
-    public class BasicAuthImpl : IUserAuthRepository
-    {
-        public UserAuth CreateUserAuth(UserAuth newUser, string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public UserAuth UpdateUserAuth(UserAuth existingUser, UserAuth newUser, string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public UserAuth GetUserAuthByUserName(string userNameOrEmail)
-        {
-            throw new NotImplementedException();
-        }
-
-        // TODO : Make this not be l33t hardcoded auth
-        public bool TryAuthenticate(string userName, string password, out UserAuth userAuth)
-        {
-            if (!string.IsNullOrWhiteSpace(userName) && userName.Equals("Aladdin")
-                && !string.IsNullOrWhiteSpace(password) && password.Equals("open sesame"))
-            {
-                userAuth = new UserAuth();
-                return true;
-            }
-            userAuth = null;
-            return false;
-        }
-
-        public bool TryAuthenticate(Dictionary<string, string> digestHeaders, string PrivateKey, int NonceTimeOut, string sequence,
-                                    out UserAuth userAuth)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LoadUserAuth(IAuthSession session, IOAuthTokens tokens)
-        {
-            throw new NotImplementedException();
-        }
-
-        public UserAuth GetUserAuth(string userAuthId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SaveUserAuth(IAuthSession authSession)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SaveUserAuth(UserAuth userAuth)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<UserOAuthProvider> GetUserOAuthProviders(string userAuthId)
-        {
-            return new List<UserOAuthProvider>();
-            //throw new NotImplementedException();
-        }
-
-        public UserAuth GetUserAuth(IAuthSession authSession, IOAuthTokens tokens)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string CreateOrMergeAuthSession(IAuthSession authSession, IOAuthTokens tokens)
-        {
-            throw new NotImplementedException();
         }
     }
 }
