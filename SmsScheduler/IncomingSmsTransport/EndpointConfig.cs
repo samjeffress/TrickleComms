4114b0df5b059c3d46b7c0f2d747d465988b3476
sam jeffress <samjeffress@gmail.com>
2013-12-02 21:58:27 +1100

    updated endpoint config for azureservicebus transport

--- SmsScheduler/IncomingSmsTransport/EndpointConfig.cs
+++ SmsScheduler/IncomingSmsTransport/EndpointConfig.cs
@@ -3,25 +3,20 @@ using NServiceBus.Features;
 
 namespace IncomingSmsTransport
 {
-	public class EndpointConfig : IWantCustomInitialization,IConfigureThisEndpoint, AsA_Server, UsingTransport<AzureServiceBus>
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, UsingTransport<AzureServiceBus>, IWantCustomInitialization
 	{
 	    public void Init()
 	    {
-            var configure = Configure.With()
+            Configure.Serialization.Xml();
+            Configure.With()
                 .DefaultBuilder()
                     .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"))
                     .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith("Events"))
-                    .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith("Messages"))
-                    .DefiningMessagesAs(t => t.Namespace == "SmsMessages")
-                    .DefiningMessagesAs(t => t.Namespace == "SmsTrackingMessages.Messages")
-                .Log4Net()
+                    .DefineEndpointName("IncomingSmsHandler")
                 .UnicastBus()
                     .LoadMessageHandlers();
-
-	        Configure.Features.Disable<Sagas>();
-	        Configure.Features.Disable<TimeoutManager>();
-
-            configure.CreateBus().Start();
+            Configure.Features.Disable<Sagas>();
+            Configure.Features.Disable<TimeoutManager>();
 	    }
 	}
 }
