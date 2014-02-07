2d8cf7dcd931361b58181983aa188b9514a4b0a0
sam jeffress <samjeffress@gmail.com>
2014-02-07 20:13:28 +1100

    testing timeout for smsactioner

--- SmsScheduler/SmsActionerTests/SmsActionerHandlerTestFixture.cs
+++ SmsScheduler/SmsActionerTests/SmsActionerHandlerTestFixture.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using NServiceBus;
 using NServiceBus.Testing;
 using NUnit.Framework;
 using Rhino.Mocks;
@@ -120,6 +119,137 @@ namespace SmsActionerTests
                     )
                 .When(a => a.Handle(sendOneMessageNow))
                 .AssertSagaCompletionIs(true);
+
+            smsService.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void TimeoutHandle_CheckStatus_SmsFailed()
+        {
+            var timeout = new SmsPendingTimeout();
+            var sendOneMessageNow = new SendOneMessageNow
+                {
+                    ConfirmationEmailAddress = "abc@def.com",
+                    CorrelationId = Guid.NewGuid(),
+                    SmsData = new SmsData("mobile", "message"),
+                    SmsMetaData = new SmsMetaData { Tags = new List<string> { "tag1", "tag2" }, Topic = "topic" }
+                };
+            var data = new SmsActionerData
+            {
+                Id = Guid.NewGuid(),
+                OriginalMessage = sendOneMessageNow,
+                Price = 0.06m,
+                SmsRequestId = "123"
+            };
+
+            var smsService = MockRepository.GenerateMock<ISmsService>();
+            var smsFailed = new SmsFailed("sid", "faile", "why why why");
+
+            smsService
+                .Expect(s => s.CheckStatus(data.SmsRequestId))
+                .Return(smsFailed);
+
+            Test.Initialize();
+            Test.Saga<SmsActioner.SmsActioner>()
+                .WithExternalDependencies(a =>
+                    {
+                        a.SmsService = smsService;
+                        a.Data = data;
+                    })
+                .WhenReceivesMessageFrom("somewhere")
+                    .ExpectPublish<MessageFailedSending>(message => 
+                        message.ConfirmationEmailAddress == sendOneMessageNow.ConfirmationEmailAddress &&
+                        message.CorrelationId == sendOneMessageNow.CorrelationId &&
+                        message.SmsData == sendOneMessageNow.SmsData && 
+                        message.SmsMetaData == sendOneMessageNow.SmsMetaData &&
+                        message.SmsFailed == smsFailed
+                    )
+                .When(a => a.Timeout(timeout))
+                .AssertSagaCompletionIs(true);
+
+            smsService.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void TimeoutHandle_CheckStatus_SmsSent()
+        {
+            var timeout = new SmsPendingTimeout();
+            var sendOneMessageNow = new SendOneMessageNow
+                {
+                    ConfirmationEmailAddress = "abc@def.com",
+                    CorrelationId = Guid.NewGuid(),
+                    SmsData = new SmsData("mobile", "message"),
+                    SmsMetaData = new SmsMetaData { Tags = new List<string> { "tag1", "tag2" }, Topic = "topic" }
+                };
+            var data = new SmsActionerData
+            {
+                Id = Guid.NewGuid(),
+                OriginalMessage = sendOneMessageNow,
+                Price = 0.06m,
+                SmsRequestId = "123"
+            };
+
+            var smsService = MockRepository.GenerateMock<ISmsService>();
+            var smsSent = new SmsSent(new SmsConfirmationData("doesn't matter", DateTime.Now, 0));
+
+            smsService
+                .Expect(s => s.CheckStatus(data.SmsRequestId))
+                .Return(smsSent);
+
+            Test.Initialize();
+            Test.Saga<SmsActioner.SmsActioner>()
+                .WithExternalDependencies(a =>
+                    {
+                        a.SmsService = smsService;
+                        a.Data = data;
+                    })
+                .WhenReceivesMessageFrom("somewhere")
+                    .ExpectPublish<MessageSent>(message => 
+                        message.ConfirmationEmailAddress == sendOneMessageNow.ConfirmationEmailAddress &&
+                        message.CorrelationId == sendOneMessageNow.CorrelationId &&
+                        message.SmsData == sendOneMessageNow.SmsData && 
+                        message.SmsMetaData == sendOneMessageNow.SmsMetaData &&
+                        message.ConfirmationData.Price == data.Price && 
+                        message.ConfirmationData.Receipt == data.SmsRequestId && 
+                        message.ConfirmationData.SentAtUtc == smsSent.SmsConfirmationData.SentAtUtc
+                    )
+                .When(a => a.Timeout(timeout))
+                .AssertSagaCompletionIs(true);
+
+            smsService.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void TimeoutHandle_CheckStatus_SmsQueued_RequestsTimeout()
+        {
+            var timeout = new SmsPendingTimeout();
+            var sendOneMessageNow = new SendOneMessageNow();
+            var data = new SmsActionerData
+            {
+                Id = Guid.NewGuid(),
+                OriginalMessage = sendOneMessageNow,
+                Price = 0.06m,
+                SmsRequestId = "123"
+            };
+
+            var smsService = MockRepository.GenerateMock<ISmsService>();
+            var smsQueued = new SmsQueued(data.SmsRequestId);
+
+            smsService
+                .Expect(s => s.CheckStatus(data.SmsRequestId))
+                .Return(smsQueued);
+
+            Test.Initialize();
+            Test.Saga<SmsActioner.SmsActioner>()
+                .WithExternalDependencies(a =>
+                    {
+                        a.SmsService = smsService;
+                        a.Data = data;
+                    })
+                .WhenReceivesMessageFrom("somewhere")
+                    .ExpectTimeoutToBeSetIn<SmsPendingTimeout>((timeoutMessage, timespan) => timespan == TimeSpan.FromSeconds(10))
+                .When(a => a.Timeout(timeout))
+                .AssertSagaCompletionIs(false);
 
             smsService.VerifyAllExpectations();
         }
