51bfadfc5e2123bb04b9ed0926c987a69a383b82
sam jeffress <samjeffress@gmail.com>
2014-02-08 13:32:03 +1100

    added implementation (partial) for ISmsTechWrapper

---
+++ SmsScheduler/ConfigurationModels/SmsTechConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConfigurationModels
+{
+    public class SmsTechConfiguration
+    {
+        [Required]
+        public string ApiSecret { get; set; }
+
+        [Required]
+        public string ApiKey { get; set; }
+
+        [Required]
+        public string From { get; set; }
+    }
+}
--- SmsScheduler/SmsActioner/ISmsTechWrapper.cs
+++ SmsScheduler/SmsActioner/ISmsTechWrapper.cs
@@ -1,4 +1,6 @@
-﻿using TransmitSms.Models.Sms;
+﻿using System;
+using ConfigurationModels;
+using TransmitSms.Models.Sms;
 
 namespace SmsActioner
 {
@@ -6,5 +8,36 @@ namespace SmsActioner
     {
         SendSmsResponse SendSmsMessage(string to, string message);
         SmsSentResponse CheckMessage(string sid);
+    }
+
+    public class SmsTechWrapper : ISmsTechWrapper
+    {
+        private IRavenDocStore DocumentStore { get; set; }
+
+        private TransmitSms.TransmitSmsWrapper TransmitSmsClient { get; set; }
+
+        public SmsTechWrapper(IRavenDocStore documentStore)
+        {
+            DocumentStore = documentStore;
+            using (var session = DocumentStore.GetStore().OpenSession("Configuration"))
+            {
+                var smsTechConfiguration = session.Load<SmsTechConfiguration>("SmsTechConfig");
+                if (smsTechConfiguration == null)
+                {
+                    throw new ArgumentException("Could not find sms tech configuration");
+                }
+                TransmitSmsClient = new TransmitSms.TransmitSmsWrapper(smsTechConfiguration.ApiKey, smsTechConfiguration.ApiSecret);
+            }
+        }
+
+        public SendSmsResponse SendSmsMessage(string to, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SmsSentResponse CheckMessage(string sid)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
