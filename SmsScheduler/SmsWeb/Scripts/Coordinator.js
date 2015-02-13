6b84ee645de28cbc418c35f12b904de4a600ffed
Sam Jeffress <samjeffress@gmail.com>
2015-02-13 20:52:38 +1100

    removing 'separated by time' option from ui

--- SmsScheduler/SmsWeb/Scripts/Coordinator.js
+++ SmsScheduler/SmsWeb/Scripts/Coordinator.js
@@ -1,52 +1,13 @@
 ﻿
 function ShowSendAll() {
-    $('#TimeSeparatedDiv').hide(200);
-    $('#SendAllByDiv').show(200);
-    $('#TimeSeparatorSeconds').val('');
     $('#SendAllAtOnce').val(false);
-    $("#OvernightWarning").hide();
     SetHighlightedButton("#SendAllByButton");
 }
 
-function ShowTimeSeparated() {
-    $('#SendAllByDiv').hide(200);
-    $('#TimeSeparatedDiv').show(200);
-    $('#SendAllBy').val('');
-    $('#SendAllAtOnce').val(false);
-    $("#OvernightWarning").hide();
-    SetHighlightedButton("#TimeSeparatedButton");
-}
-
-function HideTimingOptions() {
-    $('#SendAllByDiv').hide(200);
-    $('#TimeSeparatedDiv').hide(200);
-    $('#SendAllBy').val('');
-    $('#SendAllAtOnce').val(true);
-    $("#OvernightWarning").hide();
-    SetHighlightedButton("#AllAtOnceButton");
-}
-
-function CheckSendAll() {
-    if ($('#SendAllBy').val().length == 0) {
-        $('#SendAllByDiv').hide();
-    } else {
-        ShowSendAll();
-    }
-}
-
-function CheckTimeSeparated() {
-    if ($('#TimeSeparatorSeconds').val().length == 0) {
-        $('#TimeSeparatedDiv').hide();
-    } else {
-        ShowTimeSeparated();
-    }
-}
 
 function SetHighlightedButton(idToHighlight) {
     $('#SendAllByButton').removeClass("SelectedButton");
     $('#SendAllByButton').addClass("UnSelectedButton");
-    $('#TimeSeparatedButton').removeClass("SelectedButton");
-    $('#TimeSeparatedButton').addClass("UnSelectedButton");
     $('#AllAtOnceButton').removeClass("SelectedButton");
     $('#AllAtOnceButton').addClass("UnSelectedButton");
     
@@ -59,22 +20,7 @@ function CheckIfMessagesWillBeSentOvernight() {
         var endDate;
         var startDateTime = $.datepicker.parseDate("dd/mm/yy", $('#StartTime').val());
         var startDate = new Date(startDateTime.getFullYear(), startDateTime.getMonth(), startDateTime.getDate());
-        if ($('#TimeSeparatorSeconds').length > 0 && $('#TimeSeparatorSeconds').val().length > 0) {
-            // calculate the end date
-            var timeBetweenMessages = $('#TimeSeparatorSeconds').val();
-            var numberOfMessages = MobileNumberCount();
-            var secondsToSendAllMessages = numberOfMessages * timeBetweenMessages;
-            var endDateTime = new Date();
-            endDateTime.setTime(endDateTime.getTime() + secondsToSendAllMessages * 1000);
-            endDate = new Date(endDateTime.getFullYear(), endDateTime.getMonth(), endDateTime.getDate());
-            if (startDate < endDate) {
-                $("#OvernightWarning").removeClass("blockHidden");
-                $("#OvernightWarning").show(200);
-            } else {
-                $("#OvernightWarning").addClass("blockHidden");
-                $("#OvernightWarning").hide(200);
-            }
-        } else if ($('#SendAllBy').val().length > 0) {
+        if ($('#SendAllBy').val().length > 0) {
             // Check if the dates send messages overnight
             var endDateTime = $.datepicker.parseDate("dd/mm/yy", $('#SendAllBy').val());
             endDate = new Date(endDateTime.getFullYear(), endDateTime.getMonth(), endDateTime.getDate());
