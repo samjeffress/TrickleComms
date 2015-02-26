cd93df9b7590e9e00e773e28e73f077e7a0fb290
Sam Jeffress <samjeffress@gmail.com>
2015-02-26 20:13:30 +1100

    fixed method not in class :)

--- SmsScheduler/ConfigurationModels/CommunicationTemplate.cs
+++ SmsScheduler/ConfigurationModels/CommunicationTemplate.cs
@@ -8,17 +8,17 @@ namespace ConfigurationModels
 		public string EmailContent { get; set; }
 		public string SmsContent { get; set; }
 		public List<TemplateVariable> TemplateVariables { get; set; }
+
+		public bool ValidEmailTemplate()
+		{
+			// check variables are correct mostly
+			return true;
+		}
 	}
 
 	public class TemplateVariable
 	{
 		public string TemplateName { get; set; }
 		public bool Mandatory { get; set; }
-	}
-
-	public bool ValidEmailTemplate()
-	{
-		// check variables are correct mostly
-		return true;
 	}
 }
