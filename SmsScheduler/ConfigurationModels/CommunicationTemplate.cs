f6e4d99d091739888af784d663df5f7f6927342f
Sam Jeffress <s.jeffress@nib.com.au>
2015-04-05 21:45:02 +1000

    adding logic to extract variables from content - just email so far

--- SmsScheduler/ConfigurationModels/CommunicationTemplate.cs
+++ SmsScheduler/ConfigurationModels/CommunicationTemplate.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConfigurationModels
 {
 	public class CommunicationTemplate
 	{
+	    public CommunicationTemplate()
+	    {
+	        TemplateVariables = new List<TemplateVariable>();
+	    }
+
 		public string TemplateName { get; set; }
 		public string EmailContent { get; set; }
 		public string SmsContent { get; set; }
@@ -14,11 +21,62 @@ namespace ConfigurationModels
 			// check variables are correct mostly
 			return true;
 		}
+
+	    public void ExtractVariables()
+	    {
+	        if (!string.IsNullOrWhiteSpace(EmailContent))
+	        {
+	            var startIndexes = EmailContent.IndexesOf("{");
+	            var endIndexes = EmailContent.IndexesOf("}");
+
+	            if (startIndexes.Count != endIndexes.Count)
+	            {
+                    throw new Exception("Odd number of opening and closing brackets in template");
+	            }
+
+	            for (int i = 0; i < startIndexes.Count; i++)
+	            {
+	                if (startIndexes[i] > endIndexes[i])
+	                {
+                        throw new Exception("Brackets must be open and closed before creating new ones");
+	                }
+	                if (i != 0)
+	                {
+	                    if (startIndexes[i] < endIndexes[i-1])
+                            throw new Exception("Brackets must be open and closed before creating new ones");
+	                }
+	            }
+
+                // assuming equal
+
+	            for (var i = 0; i < startIndexes.Count; i++)
+	            {
+	                int variableLength = endIndexes[i] - (startIndexes[i] + 1);
+                    TemplateVariables.Add(new TemplateVariable { VariableName = EmailContent.Substring(startIndexes[i]+1, variableLength) });
+	            }
+	        }
+	    }
 	}
+
+    public static class StringExtension
+    {
+        public static List<int> IndexesOf(this string content, string instance)
+        {
+            var indexes = new List<int>();
+            for (var index = 0; ; index += instance.Length)
+            {
+                index = content.IndexOf(instance, index, StringComparison.Ordinal);
+                if (index == -1)
+                    break;
+                indexes.Add(index);
+            }
+            return indexes;
+        }
+    }
 
 	public class TemplateVariable
 	{
-		public string TemplateName { get; set; }
+		public string VariableName { get; set; }
 		public bool Mandatory { get; set; }
 	}
 }
---
+++ SmsScheduler/SmsWebTests/CommunicationTemplateTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using ConfigurationModels;
+using NUnit.Framework;
+
+namespace SmsWebTests
+{
+    [TestFixture]
+    public class CommunicationTemplateTestFixture
+    {
+        [Test]
+        public void EmailContainsCurlyBracketUsesVariableName()
+        {
+            var communicationTemplate = new CommunicationTemplate();
+            communicationTemplate.EmailContent = "<stuff>{var1}</stuff><p>{var2}</p>";
+
+            communicationTemplate.ExtractVariables();
+
+            Assert.That(communicationTemplate.TemplateVariables[0].VariableName, Is.EqualTo("var1"));
+            Assert.That(communicationTemplate.TemplateVariables[1].VariableName, Is.EqualTo("var2"));
+            Assert.That(communicationTemplate.TemplateVariables.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void EmailContainsUnevenNumberOfCurlyBracketsRaisesException()
+        {
+            var communicationTemplate = new CommunicationTemplate();
+            communicationTemplate.EmailContent = "<stuff>{var1}}</stuff><p>{var2}</p>";
+
+            Assert.That(() => communicationTemplate.ExtractVariables(),
+                Throws.Exception.With.Message.EqualTo("Odd number of opening and closing brackets in template"));
+        }
+
+        [Test]
+        public void EmailContainsOverlappingOpeningAndClosingBrackets_RaisesException()
+        {
+            var communicationTemplate = new CommunicationTemplate();
+            communicationTemplate.EmailContent = "<stuff>{var1</stuff><p>{var2}}</p>";
+
+            Assert.That(() => communicationTemplate.ExtractVariables(),
+                Throws.Exception.With.Message.EqualTo("Brackets must be open and closed before creating new ones"));
+        }
+
+        [Test]
+        public void EmailContainsOutOfOrderOpeningAndClosingBrackets_RaisesException()
+        {
+            var communicationTemplate = new CommunicationTemplate();
+            communicationTemplate.EmailContent = "<stuff>}{var1</stuff><p>{var2}</p>";
+
+            Assert.That(() => communicationTemplate.ExtractVariables(),
+                Throws.Exception.With.Message.EqualTo("Brackets must be open and closed before creating new ones"));
+        }
+    }
+}
