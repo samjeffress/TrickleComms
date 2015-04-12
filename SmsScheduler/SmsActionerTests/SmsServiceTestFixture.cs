90d87f8ce9381702753957396e856445de46438d
Sam Jeffress <s.jeffress@nib.com.au>
2015-04-12 19:27:12 +1000

    fixed tests - now requiring raven to determine which provider to use

--- SmsScheduler/SmsActionerTests/SmsServiceTestFixture.cs
+++ SmsScheduler/SmsActionerTests/SmsServiceTestFixture.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using ConfigurationModels;
 using NUnit.Framework;
+using Raven.Client;
 using Rhino.Mocks;
 using SmsActioner;
 using SmsMessages.CommonData;
@@ -11,12 +13,29 @@ namespace SmsActionerTests
     [TestFixture]
     public class SmsServiceTestFixture
     {
+        private IRavenDocStore _ravenDocStore;
+        private IDocumentSession _docSession;
+        private readonly SmsProviderConfiguration _twilioProvider = new SmsProviderConfiguration {SmsProvider = SmsProvider.Twilio};
+
+        [SetUp]
+        public void Setup()
+        {
+            _ravenDocStore = MockRepository.GenerateStub<IRavenDocStore>();
+            var mockRavenStore = MockRepository.GenerateStub<IDocumentStore>();
+            _docSession = MockRepository.GenerateStub<IDocumentSession>();
+            _ravenDocStore.Expect(r => r.GetStore()).Return(mockRavenStore);
+            _ravenDocStore.Expect(r => r.ConfigurationDatabaseName()).Return("something");
+            mockRavenStore.Expect(m => m.OpenSession(Arg<string>.Is.Anything)).Return(_docSession);
+        }
+
         [Test]
         public void SmsServiceSuccess()
         {
+            _docSession.Expect(d => d.Load<SmsProviderConfiguration>("SmsProviderConfiguration")).Return(_twilioProvider);
+
             var messageToSend = new SendOneMessageNow { SmsData = new SmsData("mobile", "message")};
             var twilioWrapper = MockRepository.GenerateMock<ITwilioWrapper>();
-            var smsService = new SmsService { TwilioWrapper = twilioWrapper };
+            var smsService = new SmsService { TwilioWrapper = twilioWrapper, RavenDocStore = _ravenDocStore };
 
             var smsMessage = new SMSMessage { Status = "sent", Sid = "sidReceipt", DateSent = DateTime.Now, Price = 3 };
             twilioWrapper
@@ -37,9 +56,11 @@ namespace SmsActionerTests
         [Test]
         public void SmsServiceSending()
         {
+            _docSession.Expect(d => d.Load<SmsProviderConfiguration>("SmsProviderConfiguration")).Return(_twilioProvider);
+
             var messageToSend = new SendOneMessageNow { SmsData = new SmsData("mobile", "message") };
             var twilioWrapper = MockRepository.GenerateMock<ITwilioWrapper>();
-            var smsService = new SmsService { TwilioWrapper = twilioWrapper };
+            var smsService = new SmsService { TwilioWrapper = twilioWrapper, RavenDocStore = _ravenDocStore };
 
             var smsMessageSending = new SMSMessage { Status = "sending", Sid = "sidReceipt" };
             twilioWrapper
@@ -56,9 +77,11 @@ namespace SmsActionerTests
         [Test]
         public void SmsServiceSendingFails()
         {
+            _docSession.Expect(d => d.Load<SmsProviderConfiguration>("SmsProviderConfiguration")).Return(_twilioProvider);
+
             var messageToSend = new SendOneMessageNow { SmsData = new SmsData("mobile", "message") };
             var twilioWrapper = MockRepository.GenerateMock<ITwilioWrapper>();
-            var smsService = new SmsService { TwilioWrapper = twilioWrapper };
+            var smsService = new SmsService { TwilioWrapper = twilioWrapper, RavenDocStore = _ravenDocStore };
 
             var smsMessageSending = new SMSMessage { Status = "failed", Sid = "sidReceipt", RestException = new RestException {Code = "code", Message = "message", MoreInfo = "moreInfo", Status = "status"}};
             twilioWrapper
@@ -80,9 +103,11 @@ namespace SmsActionerTests
         [Test]
         public void SmsServiceRestException()
         {
+            _docSession.Expect(d => d.Load<SmsProviderConfiguration>("SmsProviderConfiguration")).Return(_twilioProvider);
+
             var messageToSend = new SendOneMessageNow { SmsData = new SmsData("mobile", "message") };
             var twilioWrapper = MockRepository.GenerateMock<ITwilioWrapper>();
-            var smsService = new SmsService { TwilioWrapper = twilioWrapper };
+            var smsService = new SmsService { TwilioWrapper = twilioWrapper, RavenDocStore = _ravenDocStore };
 
             var smsMessageSending = new SMSMessage { RestException = new RestException {Code = "code", Message = "message", MoreInfo = "moreInfo", Status = "status"}};
             twilioWrapper
@@ -104,9 +129,11 @@ namespace SmsActionerTests
         [Test]
         public void SmsServiceMessageQueued()
         {
+            _docSession.Expect(d => d.Load<SmsProviderConfiguration>("SmsProviderConfiguration")).Return(_twilioProvider);
+
             var messageToSend = new SendOneMessageNow { SmsData = new SmsData("mobile", "message") };
             var twilioWrapper = MockRepository.GenerateMock<ITwilioWrapper>();
-            var smsService = new SmsService { TwilioWrapper = twilioWrapper };
+            var smsService = new SmsService { TwilioWrapper = twilioWrapper, RavenDocStore = _ravenDocStore };
 
             var smsMessageSending = new SMSMessage { Status = "queued", Sid = "sidReceipt" };
             twilioWrapper
