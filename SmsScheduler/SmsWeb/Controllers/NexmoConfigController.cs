fa336a992e39ea76f0fdb7f9c25a5c7286add4e8
Sam Jeffress <s.jeffress@nib.com.au>
2015-04-14 19:19:55 +1000

    added nexmo controller / view

---
+++ SmsScheduler/SmsWeb/Controllers/NexmoConfigController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+using ConfigurationModels;
+
+namespace SmsWeb.Controllers
+{
+    public class NexmoConfigController : Controller
+    {
+        public IRavenDocStore DocumentStore { get; set; }
+
+        public PartialViewResult DetailsAjax()
+        {
+            using (var session = DocumentStore.GetStore().OpenSession("Configuration"))
+            {
+                var nexmoConfiguration = session.Load<NexmoConfiguration>("NexmoConfig");
+                if (nexmoConfiguration == null)
+                    return PartialView("_NexmoConfigEdit");
+                return PartialView("_NexmoConfigDetails", nexmoConfiguration);
+            }
+        }
+
+        public PartialViewResult EditAjax()
+        {
+            using (var session = DocumentStore.GetStore().OpenSession("Configuration"))
+            {
+                var nexmoConfiguration = session.Load<NexmoConfiguration>("NexmoConfig");
+                if (nexmoConfiguration == null)
+                    return PartialView("_NexmoConfigEdit");
+                return PartialView("_NexmoConfigEdit", nexmoConfiguration);
+            }
+        }
+
+        [HttpPost]
+        public PartialViewResult EditAjax(NexmoConfiguration configuration)
+        {
+            var isValid = TryUpdateModel(configuration);
+            if (!isValid)
+                return PartialView("_NexmoConfigEdit", configuration);
+            using (var session = DocumentStore.GetStore().OpenSession("Configuration"))
+            {
+                var nexmoConfiguration = session.Load<NexmoConfiguration>("NexmoConfig");
+                if (nexmoConfiguration == null)
+                {
+                    session.Store(configuration, "NexmoConfig");
+                }
+                else
+                {
+                    nexmoConfiguration.ApiKey = configuration.ApiKey;
+                    nexmoConfiguration.Secret = configuration.Secret;
+                    nexmoConfiguration.From = configuration.From;
+                }
+                session.SaveChanges();
+                return PartialView("_NexmoConfigDetails", configuration);
+            }
+        }
+    }
+}
